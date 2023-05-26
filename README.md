0ee652b31ef74e53fbb5436e70143c20e4bcf23e
qtfkwk <qtfkwk+kron@gmail.com>
2023-05-26 10:45:00 -0400

    1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-**Coming soon!**
+```
+use kron::*;
+
+// Get current timestamp
+let now = Kron::now();
+
+// Print it ~ "2023-05-26T13:07:42Z\n"
+println!("{now}");
+
+// Get a specific timestamp (0 = UNIX epoch)
+let ts = Kron::timestamp(626609862).unwrap();
+
+// Default format (ISO8601)
+assert_eq!(format!("{ts}"), "1989-11-09T10:17:42Z");
+assert_eq!(ts.to_string(), "1989-11-09T10:17:42Z");
+
+// Format constant
+assert_eq!(ts.format(&COMPACT).unwrap(), "19891109-101742Z");
+assert_eq!(ts.format(&ISO8601).unwrap(), "1989-11-09T10:17:42Z");
+
+// Create a custom format and use it
+let custom_fmt = KronFormat::new(
+    "[weekday repr:short] [month]/[day]/[year] [hour]:[minute]",
+).unwrap();
+assert_eq!(ts.format(&custom_fmt).unwrap(), "Thu 11/09/1989 10:17");
+
+// Use a custom format string directly
+assert_eq!(
+    ts.format_str("[hour]:[minute]:[second] [month]/[day]/[year]").unwrap(),
+    "10:17:42 11/09/1989",
+);
+```
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,76 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![doc = include_str!("../README.md")]
+
+//--------------------------------------------------------------------------------------------------
+
+use anyhow::Result;
+use lazy_static::lazy_static;
+use time::OffsetDateTime;
+
+//--------------------------------------------------------------------------------------------------
+
+pub const FMT_COMPACT: &str = "[year][month][day]-[hour][minute][second]Z";
+pub const FMT_ISO8601: &str = "[year]-[month]-[day]T[hour]:[minute]:[second]Z";
+
+//--------------------------------------------------------------------------------------------------
+
+lazy_static! {
+    pub static ref COMPACT: KronFormat = KronFormat::new(FMT_COMPACT).unwrap();
+    pub static ref ISO8601: KronFormat = KronFormat::new(FMT_ISO8601).unwrap();
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+//--------------------------------------------------------------------------------------------------
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[cfg(test)]
+mod tests;
+
+//--------------------------------------------------------------------------------------------------
+
+pub struct KronFormat {
+    fmt: Vec<time::format_description::FormatItem<'static>>,
+}
+
+impl KronFormat {
+    pub fn new(s: &'static str) -> Result<KronFormat> {
+        Ok(KronFormat {
+            fmt: time::format_description::parse(s)?,
+        })
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+
+pub struct Kron {
+    dt: OffsetDateTime,
+}
+
+impl Kron {
+    pub fn now() -> Kron {
+        Kron {
+            dt: OffsetDateTime::now_utc(),
+        }
+    }
+
+    pub fn timestamp(n: i64) -> Result<Kron> {
+        Ok(Kron {
+            dt: OffsetDateTime::from_unix_timestamp(n)?,
+        })
+    }
+
+    pub fn epoch() -> Kron {
+        Kron::timestamp(0).unwrap()
+    }
+
+    pub fn format(&self, f: &KronFormat) -> Result<String> {
+        Ok(self.dt.format(&f.fmt)?)
+    }
+
+    pub fn format_str(&self, s: &'static str) -> Result<String> {
+        self.format(&KronFormat::new(s)?)
+    }
+}
+
+impl std::fmt::Display for Kron {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.dt.format(&ISO8601.fmt).unwrap())
     }
 }
